a0755228ec3b13930add553853b508dc93d98bd9
Cmunny <connor-carpenter@comcast.net>
2018-04-04 18:05:46 -0700

    fixed the file names to be lab2.

---
+++ Part3/dsilv022_ccarp006_lab2_part3.c
@@ -0,0 +1,69 @@
+/*
+ * Part2.cpp
+ *
+ * Created: 4/4/2018 4:28:21 PM
+ * Author : Connor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRC = 0xFF; PORTC = 0x00;
+	unsigned char fuelLevel = 0x00;
+	unsigned char lowLight = 0x00;
+	unsigned char keyIn = 0x00;
+	unsigned char seatbelt = 0x00;
+	unsigned char seated = 0x00;
+	unsigned char tempC = 0x00;
+    while (1) 
+    {
+		switch(PINA & 0x0F){
+			case 0x00:	fuelLevel = 0x00;
+						lowLight = 0x01;
+						break;
+			case 0x01: 
+			case 0x02: fuelLevel = 0x20;
+						lowLight = 0x01;
+				break;
+			case 3:
+			case 4: fuelLevel = 0x30;
+					lowLight = 0x01;
+				break;
+			case 5:
+			case 6: fuelLevel = 0x38;
+					lowLight = 0x00;
+				break;
+			case 7:
+			case 8:
+			case 9: fuelLevel = 0x3C;
+					lowLight = 0x00;
+				break;
+			case 10:
+			case 11:
+			case 12: fuelLevel = 0x3E;
+					 lowLight = 0x00;
+				break;
+			case 13:
+			case 14:
+			case 15: fuelLevel = 0x3F;
+					 lowLight = 0x00;
+				break;	
+		}
+		
+		
+		tempC = fuelLevel;
+		if(lowLight)
+			tempC = tempC | 0x40;
+			
+		keyIn = PINA & 0x10;
+		seated = PINA & 0x20;
+		seatbelt = PINA & 0x40;
+		if(keyIn && seated && !seatbelt)
+			tempC = tempC | 0x80;
+		PORTC = tempC;
+    }
+}
+
